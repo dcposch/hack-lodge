39344a69c248f4be4c1d9e2e6a0fadf1fd3ac003
DC <dcposch@dcpos.ch>
2019-01-06 11:09:37 -0800

    pipes, no pipe collisions

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 const width = window.innerWidth
 const height = window.innerHeight - 120
 
-const divBird = document.querySelector('#bird')
+const elemBird = document.querySelector('#bird')
+const elemPipeU = document.querySelector('#pipe-upper')
+const elemPipeL = document.querySelector('#pipe-lower')
 
 let pressedSpace = false
 let birdY = height / 2
 let birdYVel = 0
+let pipeY = height / 2
+let pipeX = width - 100
 
 // Listen for keystrokes
 window.addEventListener('keydown', function (e) {
@@ -27,7 +31,23 @@ function frame() {
     }
     birdY += birdYVel
 
-    divBird.style.top = birdY + 'px'
+    pipeX -= 5
+    if (pipeX < 0) {
+        pipeX = width - 100
+        pipeY = Math.round(Math.random() * (height - 200))
+    }
+
+    elemBird.style.top = birdY + 'px'
+
+    elemPipeU.style.top = 0
+    elemPipeU.style.left = pipeX + 'px'
+    elemPipeU.style.width = '40px'
+    elemPipeU.style.height = pipeY + 'px'
+
+    elemPipeL.style.top = (pipeY + 160) + 'px'
+    elemPipeL.style.left = pipeX + 'px'
+    elemPipeL.style.width = '40px'
+    elemPipeL.style.height = (height - pipeY - 160) + 'px'
 
     if (birdY < height && birdY > 0) {
         window.requestAnimationFrame(frame)
